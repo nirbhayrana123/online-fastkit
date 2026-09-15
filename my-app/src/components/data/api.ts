07196344545d4a95ba20c3c33c23572fdfa40286
nirbhayrana123 <nirbhydesign@gmail.com>
2026-09-15 13:57:04 +0530

    abhi

--- my-app/src/components/data/api.ts
+++ my-app/src/components/data/api.ts
@@ -115,7 +115,7 @@ const products = [
     name: "Badam (Almonds)",
     declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
     oldPrice: 700,
-    newPrice: 680,
+    newPrice: 550,
     weight: "1 kg",
     main_img: "/images/badam.jpg",
     one_img: "/images/moong-dal.jpg",
@@ -158,6 +158,7 @@ const products = [
 
   {
     id: "11",
+    discount: "10% OFF",
     name: "Alu (Potatoes)",
     declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
     oldPrice: 22,
@@ -173,7 +174,6 @@ const products = [
 
   {
     id: "12",
-    discount: "10% OFF",
     name: "Peyaj (Onions)",
     declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
     oldPrice: 33,
@@ -189,6 +189,7 @@ const products = [
 
   {
     id: "13",
+    discount: "14% OFF",
     name: "Jeera (Cumin Seeds)",
     declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
     oldPrice: 45,
@@ -235,7 +236,6 @@ const products = [
 
   {
     id: "16",
-    discount: "14% OFF",
     name: "Dhaniya Powder (Coriander)",
     declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
     oldPrice: 220,
@@ -278,7 +278,7 @@ const products = [
     three_img: "/images/badam.jpg",
     four_img: "/images/chawal-rice.jpg",
     five_img: "/images/dhaniya-powder.jpg"
-  }, 
+  },
 
   {
     id: "20",
@@ -297,6 +297,321 @@ const products = [
   },
 
 
+///////////////////////
+
+
+ {
+    id: "1",
+    discount: "10% OFF",
+    name: "Chana Dal",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 60,
+    newPrice: 55,
+    weight: "4 kg",
+    main_img: "/images/chana-dal.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "2",
+    iscount: "17% OFF",
+    name: "Moong Chilka (Split Green Gram) Dal",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 45,
+    newPrice: 40,
+    weight: "7 kg",
+    main_img: "/images/moong-dal.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "3",
+    discount: "20% OFF",
+    name: "Urad Dhuli (White Urad Dal)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 75,
+    newPrice: 73,
+    weight: "1 kg",
+    main_img: "/images/uraddal.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "4",
+    name: "Ata (Flour)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 55,
+    newPrice: 50,
+    weight: "2 kg",
+    main_img: "/images/ata.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "5",
+    discount: "20% OFF",
+    name: "Masala",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 35,
+    newPrice: 32,
+    weight: "200 g",
+    main_img: "/images/mdh.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "6",
+    name: "Chini (Sugar)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 50,
+    newPrice: 47,
+    weight: "1 kg",
+    main_img: "/images/chini.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "7",
+    discount: "10% OFF",
+    name: "Tel (Oil)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 55,
+    newPrice: 53,
+    weight: "1 kg",
+    main_img: "/images/oil.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "8",
+    name: "Badam (Almonds)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 700,
+    newPrice: 550,
+    weight: "1 kg",
+    main_img: "/images/badam.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "9",
+    discount: "10% OFF",
+    name: "Chawal (Rice)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 60,
+    newPrice: 55,
+    weight: "1 kg",
+    main_img: "/images/chawal-rice.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "10",
+    name: "Chaa (Tea)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 83,
+    newPrice: 80,
+    weight: "250 g",
+    main_img: "/images/tea-tata.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "11",
+    discount: "10% OFF",
+    name: "Alu (Potatoes)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 22,
+    newPrice: 20,
+    weight: "1 kg",
+    main_img: "/images/alu.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "12",
+    name: "Peyaj (Onions)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 33,
+    newPrice: 30,
+    weight: "1 kg",
+    main_img: "/images/peyaz.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "13",
+    discount: "14% OFF",
+    name: "Jeera (Cumin Seeds)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 45,
+    newPrice: 44,
+    weight: "250 g",
+    main_img: "/images/Jeera.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "14",
+    name: "Haldi (Turmeric)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 500,
+    newPrice: 490,
+    weight: "1 kg",
+    main_img: "/images/Haldi.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "15",
+    discount: "10% OFF",
+    name: "Lal Mirch (Red Chili)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 40,
+    newPrice: 39,
+    weight: "250 g",
+    main_img: "/images/powder-lal-mirch.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "16",
+    name: "Dhaniya Powder (Coriander)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 220,
+    newPrice: 200,
+    weight: "500 g",
+    main_img: "/images/dhaniya-powder.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "17",
+    discount: "14% OFF",
+    name: "Namak (Salt)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 30,
+    newPrice: 27,
+    weight: "1 kg",
+    main_img: "/images/tata.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "18",
+    discount: "5% OFF",
+    name: "Kaju (Cashews)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    newPrice: 300,
+    weight: "250 g",
+    main_img: "/images/kaju.jpg",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+  {
+    id: "20",
+    discount: "17% OFF",
+    name: "Anjeer (Figs)",
+    declaration: "Get items delivered to your doorstep from dark stores near you, whenever you need them.",
+    oldPrice: 360,
+    newPrice: 340,
+    weight: "300 g",
+    main_img: "/images/anjeer.webp",
+    one_img: "/images/moong-dal.jpg",
+    two_img: "/images/uraddal.jpg",
+    three_img: "/images/badam.jpg",
+    four_img: "/images/chawal-rice.jpg",
+    five_img: "/images/dhaniya-powder.jpg"
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 ];
 
 export default products;
--- my-app/src/components/pages/Home.tsx
+++ my-app/src/components/pages/Home.tsx
@@ -1,8 +1,8 @@
 import products from "../data/api";
 import { useNavigate } from "react-router-dom";
-  const handleScroll = () => {
-    document.getElementById("shopnow")?.scrollIntoView({ behavior: "smooth" });
-  };
+const handleScroll = () => {
+  document.getElementById("shopnow")?.scrollIntoView({ behavior: "smooth" });
+};
 
 export default function Home({ addToCart, search, cart, decreaseQty }: any) {
   const navigate = useNavigate();
@@ -21,11 +21,95 @@ export default function Home({ addToCart, search, cart, decreaseQty }: any) {
             <p>Best quality fruits, vegetables, and daily essentials at affordable prices. Shop now and get fast delivery with great discounts</p>
             <button onClick={handleScroll} className="cart-btn">Shop Now</button>
           </div>
-          <img src="./images/mainbanner.png" />
         </div>
       </section>
 
-      <h2  id="shopnow">Bestsellers</h2>
+      <h2 id="shopnow">Bestsellers</h2>
+      <section className="category-section">
+        <div className="category-wrapper">
+
+          <div className="cat-card" style={{ backgroundColor: '#f2fce4' }}>
+            <div className="img-holder">
+              <img src="https://cdn-icons-png.flaticon.com/512/3081/3081949.png" alt="Cake & Milk" />
+            </div>
+            <h3>Cake & Milk</h3>
+            <span>26 items</span>
+          </div>
+
+          <div className="cat-card" style={{ backgroundColor: '#fffceb' }}>
+            <div className="img-holder">
+              <img src="https://cdn-icons-png.flaticon.com/512/888/888634.png" alt="Oganic Kiwi" />
+            </div>
+            <h3>Oganic Kiwi</h3>
+            <span>28 items</span>
+          </div>
+
+          <div className="cat-card" style={{ backgroundColor: '#ecffec' }}>
+            <div className="img-holder">
+              <img src="https://cdn-icons-png.flaticon.com/512/3137/3137044.png" alt="Peach" />
+            </div>
+            <h3>Peach</h3>
+            <span>14 items</span>
+          </div>
+
+          <div className="cat-card" style={{ backgroundColor: '#feefea' }}>
+            <div className="img-holder">
+              <img src="https://cdn-icons-png.flaticon.com/512/415/415733.png" alt="Red Apple" />
+            </div>
+            <h3>Red Apple</h3>
+            <span>54 items</span>
+          </div>
+
+          <div className="cat-card" style={{ backgroundColor: '#fff3eb' }}>
+            <div className="img-holder">
+              <img src="https://cdn-icons-png.flaticon.com/512/2553/2553691.png" alt="Snack" />
+            </div>
+            <h3>Snack</h3>
+            <span>56 items</span>
+          </div>
+
+          <div className="cat-card" style={{ backgroundColor: '#fff3ff' }}>
+            <div className="img-holder">
+              <img src="https://cdn-icons-png.flaticon.com/512/2347/2347019.png" alt="Vegetables" />
+            </div>
+            <h3>Vegetables</h3>
+            <span>72 items</span>
+          </div>
+
+          <div className="cat-card" style={{ backgroundColor: '#edf9ee' }}>
+            <div className="img-holder">
+              <img src="https://cdn-icons-png.flaticon.com/512/590/590685.png" alt="Strawberry" />
+            </div>
+            <h3>Strawberry</h3>
+            <span>36 items</span>
+          </div>
+
+          <div className="cat-card" style={{ backgroundColor: '#fdeedd' }}>
+            <div className="img-holder">
+              <img src="https://cdn-icons-png.flaticon.com/512/3137/3137024.png" alt="Black plum" />
+            </div>
+            <h3>Black plum</h3>
+            <span>123 items</span>
+          </div>
+
+          <div className="cat-card" style={{ backgroundColor: '#fffceb' }}>
+            <div className="img-holder">
+              <img src="https://cdn-icons-png.flaticon.com/512/1147/1147822.png" alt="Custard apple" />
+            </div>
+            <h3>Custard apple</h3>
+            <span>34 items</span>
+          </div>
+
+          <div className="cat-card" style={{ backgroundColor: '#feefea' }}>
+            <div className="img-holder">
+              <img src="https://cdn-icons-png.flaticon.com/512/2935/2935307.png" alt="Coffe & Tea" />
+            </div>
+            <h3>Coffe & Tea</h3>
+            <span>89 items</span>
+          </div>
+
+        </div>
+      </section>
       <div className="product-row">
         {filtered.map((p) => (
           <div className="product-card">
@@ -40,9 +124,10 @@ export default function Home({ addToCart, search, cart, decreaseQty }: any) {
                 <h4>{p.name}</h4>
               </div>
               <p>{p.weight}</p>
+              <div className="price-row">
               <div className="pricebox">
-                <p className="oldprice">₹{p.oldPrice}</p>
                 <p className="price">₹{p.newPrice}</p>
+                <p className="oldprice">₹{p.oldPrice}</p> 
               </div>
 
 
@@ -56,15 +141,8 @@ export default function Home({ addToCart, search, cart, decreaseQty }: any) {
                     <button onClick={() => addToCart(p)}>+</button>
                   </div>
                 ) : (
-                  <button
-                    className="add-btn"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      addToCart(p);
-                    }}
-                  >
-                    Add
-                  </button>
+                  <button className="add-btn"onClick={(e) => {e.stopPropagation();addToCart(p); }}><i className="fa-solid fa-cart-shopping"></i> Add</button>
+                  
                 );
               })()}
 
@@ -73,8 +151,9 @@ export default function Home({ addToCart, search, cart, decreaseQty }: any) {
             </div>
 
           </div>
+          </div>
         ))}
-      </div> 
+      </div>
     </div>
   );
 }
